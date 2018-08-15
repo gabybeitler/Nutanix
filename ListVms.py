acd254a0bf3f806b15004b1bcce7083a9aa51b9d
gabybeitler <gabybeitler@gmail.com>
2018-08-15 19:38:16 -0400

    Add files via upload

---
+++ ListVms.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+
+username =
+password =
+payload = '{"kind":"vm"}'
+headers = {"content-type": "application/json"}
+
+requests.packages.urllib3.disable_warnings()
+
+response = requests.post('https://10.21.38.37:9440/api/nutanix/v3/vms/list',
+    verify = False,
+    auth = (username, password),
+    data = payload,
+    headers = headers)
+
+output = json.loads(response.text)
+
+nvm = output["metadata"]["total_matches"]
+
+vm = [item for item in output["entities"]
+    if item["metadata"]["kind"] == "vm"]
+
+x = 0
+
+print('{0:15} : {1:20}'.format('Virtual Machine', 'UUID'))
+print('-'*55)
+
+while x < nvm:
+    print('{0:15} : {1:20}'.format(vm[x]["spec"]["name"],
+        vm[x]["metadata"]["uuid"]))
+    x = x+1
